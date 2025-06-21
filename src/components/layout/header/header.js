93755b06adffed17e005797444aced9df428aef3
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-06-21 22:51:53 +0200

    added hero section

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,1 +1,12 @@
-import './header.scss'
+import './header.scss';
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.header');
+  const container = header.querySelector('.header__container');
+
+  if (window.scrollY > 30) {
+    container.classList.add('scrolled');
+  } else {
+    container.classList.remove('scrolled');
+  }
+});
