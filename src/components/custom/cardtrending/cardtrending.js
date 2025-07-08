44e48c46bd2b92d685698e76ab30c49cafbbbc15
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-07-08 19:22:20 +0200

    added click for card-trending

--- src/components/custom/cardtrending/cardtrending.js
+++ src/components/custom/cardtrending/cardtrending.js
@@ -1,1 +1,11 @@
-import "./cardtrending.scss"
+import './cardtrending.scss';
+
+document.querySelectorAll('.action-cardtrending__item--icon-mark-cart').forEach((item) =>
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    item.classList.toggle('action-cardtrending__item--icon-mark-cart--active');
+
+    item.classList.toggle('active');
+  })
+);
